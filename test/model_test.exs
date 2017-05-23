dea3323055e114d44719d72d86e74b49e7f8505a
Craig Spaeth <craigspaeth@gmail.com>
2017-05-23 19:21:06 -0400

    Wrote failing test for nested validation

--- test/model_test.exs
+++ test/model_test.exs
@@ -8,4 +8,24 @@ defmodule GriffinModelTest do
     assert Griffin.Model.validate foo, schema
     assert not Griffin.Model.validate bar, schema
   end
+
+  test "validates a nested map of fields with vex" do
+    schema = %{
+      name: [:string],
+      location: %{
+        city: [:string],
+        country: [:string, [presence: :true]],
+        state: [:string]
+      }
+    }
+    cincinnati = %{
+      name: "present",
+      location: %{
+        city: "Cincinnati",
+        country: "USA",
+        state: "Ohio"
+      }
+    }
+    assert Griffin.Model.validate cincinnati, schema
+  end
 end
