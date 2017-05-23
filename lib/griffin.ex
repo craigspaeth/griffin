26d01cdbbed4c670ffed2c390d85812f08e36c40
Artsy <craigspaeth@gmail.com>
2017-05-22 23:43:20 -0400

    Init project

---
+++ lib/griffin.ex
@@ -0,0 +1,18 @@
+defmodule Griffin do
+  @moduledoc """
+  Documentation for Griffin.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Griffin.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/griffin_test.exs
@@ -0,0 +1,8 @@
+defmodule GriffinTest do
+  use ExUnit.Case
+  doctest Griffin
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
